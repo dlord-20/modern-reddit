1980e0711650b5df7c336e9ced820d48f5671c05
dlord-20 <dthomaslord@gmail.com>
2022-08-21 12:04:26 +0300

    Init commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Testing here.
+        </p>
         <span>
           <span>Learn </span>
           <a
