d180601b94a7ad364ee1e23ac09f34e4422a4325
dlord-20 <dthomaslord@gmail.com>
2022-08-21 16:03:03 +0300

    Changing category filter changes Nav state

--- src/features/nav/Nav.js
+++ src/features/nav/Nav.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState} from "react";
 
 export default function Nav() {
+    const [ categoryFilter, setCategoryFilter ] = useState('Trending');
+
+    const handleCategoryChange = (event) => {
+        setCategoryFilter(event.target.value);
+    }
+
     return (
         <div>
             <p>Navigation</p>
+            <form>
+                <label>
+                    <select value={categoryFilter} onChange={handleCategoryChange}>
+                        <option value="Trending">Trending</option>
+                        <option value="New">New</option>
+                        <option value="Popular">Popular</option>
+                    </select>
+                </label>
+            </form>
         </div>
     )
 }
