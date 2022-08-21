ce0f71aa97ebcce46837a05dda1e180b21b1caee
dlord-20 <dthomaslord@gmail.com>
2022-08-21 16:09:17 +0300

    Added a new date range select filter in navigation -> Still need to change these to use the store instead of state

--- src/features/nav/Nav.js
+++ src/features/nav/Nav.js
@@ -2,9 +2,15 @@ import React, { useState} from "react";
 
 export default function Nav() {
     const [ categoryFilter, setCategoryFilter ] = useState('Trending');
+    const [ dateRangeFilter, setDateRangeFilter ] = useState('24 Hours');
+
 
     const handleCategoryChange = (event) => {
         setCategoryFilter(event.target.value);
+    }
+
+    const handleDateChange = (event) => {
+        setDateRangeFilter(event.target.value);
     }
 
     return (
@@ -16,6 +22,15 @@ export default function Nav() {
                         <option value="Trending">Trending</option>
                         <option value="New">New</option>
                         <option value="Popular">Popular</option>
+                    </select>
+                </label>
+                <label>
+                    <select value={dateRangeFilter} onChange={handleDateChange}>
+                        <option value="24 Hours">24 Hours</option>
+                        <option value="3 Days">3 Days</option>
+                        <option value="Week">Week</option>
+                        <option value="Month">Month</option>
+                        <option value="Year">Year</option>
                     </select>
                 </label>
             </form>
